0330964aaf081ef3d3b4e77e7cc68fecc171d35b
madniraza28 <150328481+madniraza28@users.noreply.github.com>
2024-05-20 21:20:55 +0500

    newcommitchanges

--- CaseTracker.DataAccessLayer/IDataServices/IConsultantRepo.cs
+++ CaseTracker.DataAccessLayer/IDataServices/IConsultantRepo.cs
@@ -12,15 +12,10 @@ namespace CaseTracker.DataAccessLayer.IDataServices
     {
         Task<int> Add(Consultant consultant);
         Task<Consultant?> GetById(int id);
-        Task<int> Delete(Consultant consultant);
+     
         Task<Consultant?> GetById(string consultationId);
         Task<List<Consultant>> GetAll();
-        Task<int> DeleteStep(NextSteps next);
-        Task<NextSteps?> GetByNextStepId(int consultantId);
-        Task<int> DeleteCommunicatioUpdate(CommunicationUpdates communication);
-        Task<CommunicationUpdates?> GetByCommunicationId(int consultantId);
-        Task<int> DeleteAttachments(AttachmentModel attachment);
-        Task<AttachmentModel?> GetByAttachmentId(int consultantId);
+        
 
 
     }
